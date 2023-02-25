4cf48f9cac65bee2a0bbf7451953b4b5195f7057
tiagofaraujo <tiago_araujo@hotmail.com>
2023-02-25 16:08:06 +0000

    It. 1

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function IdCard(props) {
+    const { lastName, firstName, gender, height, birth, picture} = props;
+  return (
+    <div>
+      <img src={picture} alt="profile" />
+      <p>
+        <b>First Name:</b> {firstName}
+      </p>
+      <p>
+        <b>Last Name:</b> {lastName}
+      </p>
+      <p>
+        <b>Gender:</b> {gender}
+      </p>
+      <p>
+        <b>Height:</b> {height}cm
+      </p>
+     {/*  <p><b>Birthday:</b> {props.birth}</p> */}
+      <br></br>
+    </div>
+  );
+}
+
+export default IdCard;
